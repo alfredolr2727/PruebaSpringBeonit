29014b8af56e06631d2428d32c505a7951d1cf28
alfredolr2727 <61191456+alfredolr2727@users.noreply.github.com>
2021-11-24 10:21:00 +0100

    Quitar comentario

--- src/main/java/com/prueba/Prueba/PruebaApplication.java
+++ src/main/java/com/prueba/Prueba/PruebaApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PruebaApplication {
-
-	/*
-	* Por algún problema con la version de Maven y los plugins que utiliza a la hora de ejecutar los test no he podido
-	* hacer que los ejecute todos automáticamente a través de Maven como me hubiera gustado. Y no he tenido tiempo
-	* de investigar porque ocurría esto, los he ido ejecutando uno a uno desde la propia clase de test.
-	* */
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaApplication.class, args);
 	}
